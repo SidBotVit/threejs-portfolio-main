a25a2cedeac45efaf66771bde592ed895a8972bf
SidBotVit <sidhantmalik02@gmail.com>
2025-01-09 16:23:08 +0530

    made portfolio website using three js

---
+++ README.md
@@ -0,0 +1,97 @@
+<div align="center">
+  <br />
+    <a href="#">
+      <img src="https://via.placeholder.com/800x400?text=Sidhant+Malik+Project" alt="Project Banner">
+    </a>
+  <br />
+
+  <div>
+    <img src="https://img.shields.io/badge/-React_JS-black?style=for-the-badge&logoColor=white&logo=react&color=61DAFB" alt="react.js" />
+    <img src="https://img.shields.io/badge/-Three_JS-black?style=for-the-badge&logoColor=white&logo=threedotjs&color=000000" alt="three.js" />
+    <img src="https://img.shields.io/badge/-Tailwind_CSS-black?style=for-the-badge&logoColor=white&logo=tailwindcss&color=06B6D4" alt="tailwindcss" />
+  </div>
+
+  <h3 align="center">Sidhant Malik's 3D Portfolio Project</h3>
+
+   <div align="center">
+     A unique and engaging 3D portfolio project showcasing advanced web development skills. Built with React, Three.js, and Tailwind CSS.
+   </div>
+</div>
+
+## 📋 Table of Contents
+
+1. 🤖 [Introduction](#introduction)
+2. ⚙️ [Tech Stack](#tech-stack)
+3. 🔋 [Features](#features)
+4. 🤸 [Quick Start](#quick-start)
+5. 🕸️ [Snippets (Code Examples)](#snippets)
+6. 📦 [Assets](#assets)
+7. 📬 [Contact](#contact)
+
+## 🤖 Introduction
+
+Welcome to my 3D portfolio project, where I explore the intersection of creativity and technology. This project is a dynamic, fully responsive website demonstrating immersive 3D designs and interactive elements. The goal is to create a memorable online presence while learning cutting-edge web development technologies.
+
+## ⚙️ Tech Stack
+
+- Node.js
+- React.js
+- Three.js
+- React Three Fiber
+- React Three Drei
+- Tailwind CSS
+- Vite
+
+## 🔋 Features
+
+- **Interactive 3D Hero Section**: A visually engaging hero section with mouse-responsive 3D animations.
+- **About Me**: A clean and concise section showcasing personal information, tech stack, and a 3D globe pinpointing location.
+- **Dynamic Projects Showcase**: An interactive section to display projects with live previews embedded in a 3D model.
+- **Responsive Design**: Optimized for all devices, ensuring a seamless experience on desktops and mobiles.
+
+## 🤸 Quick Start
+
+**Prerequisites**
+
+Ensure you have the following installed on your system:
+
+- [Git](https://git-scm.com/)
+- [Node.js](https://nodejs.org/en)
+- [npm](https://www.npmjs.com/) (Node Package Manager)
+
+**Cloning the Repository**
+
+```bash
+git clone https://github.com/sidhantmalik/3d-portfolio.git
+cd 3d-portfolio
+
+
+npm install
+npm run dev
+
+🕸️ Snippets
+Here are a few key code snippets:
+
+<details> <summary><code>tailwind.config.js</code></summary>
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        generalsans: ['General Sans', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
+📦 Assets
+All models and assets used in this project are open-source or custom-created. Reach out for more details.
+
+📬 Contact
+Feel free to reach out for collaborations or project discussions:
+
+Name: Sidhant Malik
+Email: sidhantmalik02@gmail.com
+GitHub: github.com/SidBotVit
+
---
+++ src/constants/index.js
@@ -0,0 +1,302 @@
+export const navLinks = [
+  {
+    id: 1,
+    name: 'Home',
+    href: '#home',
+  },
+  {
+    id: 2,
+    name: 'About',
+    href: '#about',
+  },
+  {
+    id: 3,
+    name: 'Work',
+    href: '#work',
+  },
+  {
+    id: 4,
+    name: 'Contact',
+    href: '#contact',
+  },
+];
+
+export const clientReviews = [
+  {
+    id: 1,
+    name: 'Emily Johnson',
+    position: 'Marketing Director at GreenLeaf',
+    img: 'assets/review1.png',
+    review:
+      'Working with siddhant was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.',
+  },
+  {
+    id: 2,
+    name: 'Mark Rogers',
+    position: 'Founder of TechGear Shop',
+    img: 'assets/review2.png',
+    review:
+      'siddhant’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional! Fantastic work.',
+  },
+  {
+    id: 3,
+    name: 'John Dohsas',
+    position: 'Project Manager at UrbanTech ',
+    img: 'assets/review3.png',
+    review:
+      'I can’t say enough good things about siddhant. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.',
+  },
+  {
+    id: 4,
+    name: 'Ether Smith',
+    position: 'CEO of BrightStar Enterprises',
+    img: 'assets/review4.png',
+    review:
+      'siddhant was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend backend dev are top-notch.',
+  },
+];
+
+export const myProjects = [
+  {
+    title: 'Podcastr - AI Podcast Platform',
+    desc: 'Podcastr is a revolutionary Software-as-a-Service platform that transforms the way podcasts are created. With advanced AI-powered features like text-to-multiple-voices functionality, it allows creators to generate diverse voiceovers from a single text input.',
+    subdesc:
+      'Built as a unique Software-as-a-Service app with Next.js 14, Tailwind CSS, TypeScript, Framer Motion and Convex, Podcastr is designed for optimal performance and scalability.',
+    href: 'https://www.youtube.com/watch?v=zfAb95tJvZQ',
+    texture: '/textures/project/project1.mp4',
+    logo: '/assets/project-logo1.png',
+    logoStyle: {
+      backgroundColor: '#2A1816',
+      border: '0.2px solid #36201D',
+      boxShadow: '0px 0px 60px 0px #AA3C304D',
+    },
+    spotlight: '/assets/spotlight1.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'LiveDoc - Real-Time Google Docs Clone',
+    desc: 'LiveDoc is a powerful collaborative app that elevates the capabilities of real-time document editing. As an enhanced version of Google Docs, It supports millions of collaborators simultaneously, ensuring that every change is captured instantly and accurately.',
+    subdesc:
+      'With LiveDoc, users can experience the future of collaboration, where multiple contributors work together in real time without any lag, by using Next.js and Liveblocks newest features.',
+    href: 'https://www.youtube.com/watch?v=y5vE8y_f_OM',
+    texture: '/textures/project/project2.mp4',
+    logo: '/assets/project-logo2.png',
+    logoStyle: {
+      backgroundColor: '#13202F',
+      border: '0.2px solid #17293E',
+      boxShadow: '0px 0px 60px 0px #2F6DB54D',
+    },
+    spotlight: '/assets/spotlight2.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'CarePulse - Health Management System',
+    desc: 'An innovative healthcare platform designed to streamline essential medical processes. It simplifies patient registration, appointment scheduling, and medical record management, providing a seamless experience for both healthcare providers and patients.',
+    subdesc:
+      'With a focus on efficiency, CarePulse integrantes complex forms and SMS notifications, by using Next.js, Appwrite, Twillio and Sentry that enhance operational workflows.',
+    href: 'https://www.youtube.com/watch?v=lEflo_sc82g',
+    texture: '/textures/project/project3.mp4',
+    logo: '/assets/project-logo3.png',
+    logoStyle: {
+      backgroundColor: '#60f5a1',
+      background:
+        'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
+      border: '0.2px solid rgba(208, 213, 221, 1)',
+      boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
+    },
+    spotlight: '/assets/spotlight3.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'Horizon - Online Banking Platform',
+    desc: 'Horizon is a comprehensive online banking platform that offers users a centralized finance management dashboard. It allows users to connect multiple bank accounts, monitor real-time transactions, and seamlessly transfer money to other users.',
+    subdesc:
+      'Built with Next.js 14 Appwrite, Dwolla and Plaid, Horizon ensures a smooth and secure banking experience, tailored to meet the needs of modern consumers.',
+    href: 'https://www.youtube.com/watch?v=PuOVqP_cjkE',
+    texture: '/textures/project/project4.mp4',
+    logo: '/assets/project-logo4.png',
+    logoStyle: {
+      backgroundColor: '#0E1F38',
+      border: '0.2px solid #0E2D58',
+      boxShadow: '0px 0px 60px 0px #2F67B64D',
+    },
+    spotlight: '/assets/spotlight4.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'Imaginify - AI Photo Manipulation App',
+    desc: 'Imaginify is a groundbreaking Software-as-a-Service application that empowers users to create stunning photo manipulations using AI technology. With features like AI-driven image editing, a payments system, and a credits-based model.',
+    subdesc:
+      'Built with Next.js 14, Cloudinary AI, Clerk, and Stripe, Imaginify combines cutting-edge technology with a user-centric approach. It can be turned into a side income or even a full-fledged business.',
+    href: 'https://www.youtube.com/watch?v=Ahwoks_dawU',
+    texture: '/textures/project/project5.mp4',
+    logo: '/assets/project-logo5.png',
+    logoStyle: {
+      backgroundColor: '#1C1A43',
+      border: '0.2px solid #252262',
+      boxShadow: '0px 0px 60px 0px #635BFF4D',
+    },
+    spotlight: '/assets/spotlight5.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+];
+
+export const calculateSizes = (isSmall, isMobile, isTablet) => {
+  return {
+    deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+    deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+    cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
+    reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
+    ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
+    targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+  };
+};
+
+export const workExperiences = [
+  {
+    id: 1,
+    name: 'Smart Agriculture System',
+    technologies: ['Arduino', 'Embedded C', 'IoT', 'HTML', 'CSS', 'JavaScript'],
+    duration: 'April 2024 – Present',
+    description: "Developed a responsive and interactive interface for real-time soil and wind pressure monitoring using Arduino IoT. Integrated GSM module to send alert messages.",
+    icon: '/assets/smart-agriculture.svg', // Replace with the actual path to your custom icon
+  },
+  {
+    id: 2,
+    name: 'Seamless Toll Gate',
+    technologies: ['LittleFS', 'RFID', 'ESP32', 'HTML', 'CSS', 'JavaScript', 'AWS'],
+    duration: 'Jan. 2024 – April 2024',
+    description: "Created a web application for real-time user information. Automated toll deduction using ESP32 and RFID cards.",
+    icon: '/assets/seamless-toll.svg', // Replace with the actual path to your custom icon
+  },
+  {
+    id: 3,
+    name: 'Personal Website',
+    technologies: ['ViteJS', 'ThreeJS', 'NextJS'],
+    duration: 'July 2024 – Present',
+    description: "Designed and developed a personal portfolio site showcasing projects and skills. Focused on an intuitive and visually appealing interface for enhanced user engagement.",
+    icon: '/assets/personal-website.svg', // Replace with the actual path to your custom icon
+  },
+  {
+    id: 4,
+    name: 'Movie Recommendation System',
+    technologies: ['Python', 'NLP', 'Streamlit'],
+    duration: 'May 2024 – June 2024',
+    description: "Built a movie recommender system using machine learning and cosine similarity. Deployed the system with a user-friendly interface on Streamlit.",
+    icon: '/assets/human.jpg', // Replace with the actual path to your custom icon
+  },
+  {
+    id: 1,
+    name: 'Humanoid, Vellore Institute of Technology',
+    pos: 'Special Team Member',
+    duration: 'Jan. 2024 – Sep. 2024',
+    title: "Planned and managed project timelines and milestones, delegating tasks efficiently. Led and motivated the team to achieve project goals, ensuring effective communication and collaboration. Provided technical guidance to maintain the quality and accuracy of project work.",
+    icon: '/assets/human.jpg', // Replace with the actual path to your custom icon
+    animation: 'victory',
+  },
+];
